48e16b3dcea223f62ac8405e0afecff47319671e
Kenneth Ballenegger <kenneth@ballenegger.com>
2012-04-07 23:31:08 -0700

    increment kudos counter app working

--- backend/controllers/kudos.rb
+++ backend/controllers/kudos.rb
@@ -1,8 +1,34 @@
-require 'json'
+require 'mongo'
 
 class KudosController
     
-    def index kenji
+    def initialize
+        @db = Mongo::Connection.new.db('kswizz')
+    end
+    
+    def index(kenji)
         {hello: :world}
+    end
+
+    def count(kenji, id)
+        posts = @db['posts']
+
+        post = posts.find_one({tumblr_id: id})
+        count = post['kudos'] unless post.nil?
+        count ||= 0
+        {count: count}
+    end
+
+    def increment(kenji, id)
+        posts = @db['posts']
+
+        post = posts.find_one({tumblr_id: id})
+        post = {tumblr_id: id} unless post
+        count = post['kudos']
+        count ||= 0
+        count += 1
+        post['kudos'] = count
+        posts.save(post)
+        {count: count}
     end
 end
