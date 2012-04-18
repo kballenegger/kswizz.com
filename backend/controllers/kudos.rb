00aae158717a175b61cfda1810e92d10b44fffce
Kenneth Ballenegger <kenneth@ballenegger.com>
2012-04-18 09:12:55 -0700

    memcached

--- backend/controllers/kudos.rb
+++ backend/controllers/kudos.rb
@@ -1,38 +1,53 @@
 require 'mongo'
+require 'memcache'
 
 class KudosController
     
-    def initialize
-        @db = Mongo::Connection.new.db('kswizz')
-    end
+  def initialize
+    @db = Mongo::Connection.new.db('kswizz')
+    @cache = Memcache::NativeServer.new(:server => 'localhost:11211')
+  end
     
-    def index(kenji)
-        {hello: :world}
-    end
+  def index(kenji)
+    {hello: :world}
+  end
 
-    def count(kenji, id)
-        kenji.header 'Access-Control-Allow-Origin' => '*'
+  def count(kenji, id)
+    kenji.header 'Access-Control-Allow-Origin' => '*'
         
-        posts = @db['posts']
+    key = "kudos-#{id}"
+    response = @cache.get(key) if @cache
 
-        post = posts.find_one({tumblr_id: id})
-        count = post['kudos'] unless post.nil?
-        count ||= 0
-        {count: count}
+    unless response
+      posts = @db['posts']
+
+      post = posts.find_one({tumblr_id: id})
+      count = post['kudos'] unless post.nil?
+      count ||= 0
+      response = {count: count}
+
+      @cache.set(key, response) if @cache
     end
+    response
+  end
 
-    def increment(kenji, id)
-        kenji.header 'Access-Control-Allow-Origin' => '*'
+  def increment(kenji, id)
+    kenji.header 'Access-Control-Allow-Origin' => '*'
 
-        posts = @db['posts']
+    posts = @db['posts']
 
-        post = posts.find_one({tumblr_id: id})
-        post = {tumblr_id: id} unless post
-        count = post['kudos']
-        count ||= 0
-        count += 1
-        post['kudos'] = count
-        posts.save(post)
-        {count: count}
-    end
+    post = posts.find_one({tumblr_id: id})
+    post = {tumblr_id: id} unless post
+    count = post['kudos']
+    count ||= 0
+    count += 1
+    post['kudos'] = count
+    
+    posts.save(post)
+
+    key = "kudos-#{id}"
+    @cache.delete(key) if @cache
+    
+    {count: count}
+  end
 end
